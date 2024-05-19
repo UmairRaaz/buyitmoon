72f87c01585e2fa55234a7ad8e0765a54d7e1f7f
UmairRaaz <umairgopang123@gmail.com>
2024-05-19 19:04:02 +0500

    fix local

--- src/context/ProductContextProvider.jsx
+++ src/context/ProductContextProvider.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductContext from "./ProductContext";
 
 const ProductContextProvider = ({ children }) => {
@@ -7,10 +7,19 @@ const ProductContextProvider = ({ children }) => {
     // const wishedProducts =  JSON.parse(window.localStorage.getItem("wishlist"));
     // const cartLocal =  JSON.parse(window.localStorage.getItem("cart"));
     // const cartItemLocal =  JSON.parse(window.localStorage.getItem("cartItem"))
-    const storedProducts =  JSON.parse(localStorage.getItem("products"));
-    const wishedProducts =  JSON.parse(localStorage.getItem("wishlist"));
-    const cartLocal =  JSON.parse(localStorage.getItem("cart"));
-    const cartItemLocal =  JSON.parse(localStorage.getItem("cartItem"))
+    
+
+    useEffect(()=>{
+        const storedProducts =  JSON.parse(localStorage.getItem("products"));
+        const wishedProducts =  JSON.parse(localStorage.getItem("wishlist"));
+        const cartLocal =  JSON.parse(localStorage.getItem("cart"));
+        const cartItemLocal =  JSON.parse(localStorage.getItem("cartItem"))
+
+        setProductData(storedProducts)
+        setWishlist(wishedProducts)
+        setCart(cartLocal)
+        setCartItem(cartItemLocal)
+    }, [])
 
     const [productData, setProductData] = useState(storedProducts || [])
     const [wishlist, setWishlist] = useState(wishedProducts || [])
